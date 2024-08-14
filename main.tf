5f6410f5f775967861b84fdcebf6ce5b036fb377
João Lacerda <joao.sclf@gmail.com>
2024-08-14 16:41:27 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+    required_version = ">= 1.3.0"
+    
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.114.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name = "rg-terraform-state"
+    storage_account_name = "joaoterraformstate"
+    container_name = "remote-state"
+    key = "azure-vm/terraform.tfstate"
+  }
+  
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "terraform_remote_state" "foo" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "joaoterraformstate"
+    container_name       = "remote-state"
+    key                  = "azure-vnet/terraform.tfstate"
+  }
+}
+
+
